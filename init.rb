56f620796e90a65d80f20a1c06953fd61d62a076
sdwdaw <sergiyzhbanov@gmail.com>
2021-07-02 23:21:39 +0300

    33 Reflection method_missing

--- init.rb
+++ init.rb
@@ -12,3 +12,10 @@ require_relative "order"
 @items << AntiqueItem.new({:price => 145, :weight => 35, :name => "car"})
 @items << RealItem.new({:price => 120, :weight => 75, :name => "kettle"})
 @items << RealItem.new({:price => 167, :weight => 20, :name => "dishwasher"})
+
+cart = Cart.new("sdwdaw")
+cart.add_item RealItem.new({:price => 160, :weight => 75, :name => "car"})
+cart.add_item RealItem.new({:price => 120, :weight => 75, :name => "kettle"})
+cart.add_item RealItem.new({:price => 190, :weight => 75, :name => "car"})
+p cart.all_cars
+p cart.all_kettles
