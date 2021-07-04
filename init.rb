f16103cb8262f28ff438da734cfa76c9d30453ce
sdwdaw <sergiyzhbanov@gmail.com>
2021-07-04 19:32:25 +0300

    37 Domain Specific Languages (DSL)

--- init.rb
+++ init.rb
@@ -1,7 +1,21 @@
 require_relative "store_application"
 
-puts StoreApplication.new == StoreApplication.new
-p StoreApplication.new
+StoreApplication.config do |app|
+
+  app.name = "My Store"
+  app.environment = :development
+
+  app.admin do |admin|
+    admin.email = "admin@mystore.com"
+    admin.login = "admin"
+    admin.send_info_emails_on :mondays
+  end
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
 
 @items = []
 @items<<(AntiqueItem.new "car", price: 145, weight: 35)
