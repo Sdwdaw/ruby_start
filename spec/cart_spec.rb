4bf7171f2e8ed2d1d5b1cc988e2294ac56c548a9
sdwdaw <sergiyzhbanov@gmail.com>
2021-07-05 21:58:09 +0300

    46 Rspec: nested describe & pending tests

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -7,11 +7,24 @@ require_relative "../app/cart"
 
 describe Cart do
   
-  it "adds items into thr cart" do
-    cart = Cart.new("sdwdaw")
-    item1 = Item.new("kettle", price: 200)
-    item2 = Item.new("car", price: 200)
-    cart.add_items(item1, item2)
-    cart.items.should include(item1, item2)
+  describe "managing items" do
+    
+    it "adds items into thr cart" do
+      cart = Cart.new("sdwdaw")
+      item1 = Item.new("kettle", price: 200)
+      item2 = Item.new("car", price: 200)
+      cart.add_items(item1, item2)
+      cart.items.should include(item1, item2)
+    end
+  
+    it "removes items from itself" do
+      pending("something else getting finished")
+      this_should_not_get_executed
+    end
+
   end
+  
+  it "counts items in itself"
+  it "places order using all the items that were added into the cart"
+  it "clears itself off thr items aftr an order is placed"
 end
