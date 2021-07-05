7737cdbf3ccc0e901d4faf84e08034247f788ad5
sdwdaw <sergiyzhbanov@gmail.com>
2021-07-05 22:52:10 +0300

    48-49 Procs & Lambda

---
+++ spec/real_item_spec.rb
@@ -0,0 +1,18 @@
+require "rspec"
+require_relative "../app/Item"
+require_relative "../app/virtual_item"
+require_relative "../app/real_item"
+
+describe RealItem do
+  
+  it "uses weight while returning info if the condition is true" do
+    item1 = RealItem.new(name: "kettle", price: 200, weight: 2)
+    item2 = RealItem.new(name: "kettle", price: 300, weight: 10)
+    item1_info = []
+    item2_info = []
+    item1.info { |attr| item1_info << attr }
+    item2.info { |attr| item2_info << attr }
+    item1_info.join(', ').should == "222.0, kettle"
+    item2_info.join(', ').should == "10, 332.0, kettle"
+  end
+end
