19b0c0f34f7475521b959c5270ef5c28e9e57aa1
sdwdaw <sergiyzhbanov@gmail.com>
2021-07-05 13:17:44 +0300

    40 Frozen objects

--- init.rb
+++ init.rb
@@ -12,26 +12,18 @@ StoreApplication.config do |app|
   end
 end
 
-p StoreApplication.environment
-p StoreApplication.name
+unless StoreApplication.frozen?
+  StoreApplication.name = "My name"
+end
+# StoreApplication::Admin.email = "new@email.com"
 p StoreApplication::Admin.email
-p StoreApplication::Admin.login
 
 @items = []
 @items<<(AntiqueItem.new "car", price: 145, weight: 35)
 @items << RealItem.new({:price => 120, :weight => 75, :name => "kettle"})
 @items << RealItem.new({:price => 167, :weight => 20, :name => "dishwasher"})
 
-@items.each { |i| puts i.name }
-
 cart = Cart.new("sdwdaw")
 cart.add_item RealItem.new({:price => 160, :weight => 75, :name => "car"})
 cart.add_item RealItem.new({:price => 120, :weight => 75, :name => "kettle"})
 cart.add_item RealItem.new({:price => 190, :weight => 75, :name => "car"})
-
-method = "all_cars"
-p cart.send(method)
-
-order = Order.new
-@items.each { |i| order.add_item(i) }
-order.place
